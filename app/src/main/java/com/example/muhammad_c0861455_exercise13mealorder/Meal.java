561fea510f4b6104abdd045a98ee7866e9d16298
Stainley Lebron <stainley.lebron@gmail.com>
2022-12-06 19:13:03 -0500

    Update and rename meals.java to Meal.java
    Classes must be capitalize, and array is outside the class

--- app/src/main/java/com/example/muhammad_c0861455_exercise13mealorder/Meal.java
+++ app/src/main/java/com/example/muhammad_c0861455_exercise13mealorder/Meal.java
@@ -1,22 +1,13 @@
 package com.example.muhammad_c0861455_exercise13mealorder;
 
-public class meals {
-    meals[] meals = {new meals("Biryani", 12), new meals("Chicken Burger",10), new meals("Butter Chicken",14)};
+public class Meal {
 
     private String name;
     private double price;
 
-    public meals(String name, int price) {
+    public Meal(String name, int price) {
         this.name = name;
         this.price = price;
-    }
-
-    public com.example.muhammad_c0861455_exercise13mealorder.meals[] getMeals() {
-        return meals;
-    }
-
-    public void setMeals(com.example.muhammad_c0861455_exercise13mealorder.meals[] meals) {
-        this.meals = meals;
     }
 
     public String getName() {
